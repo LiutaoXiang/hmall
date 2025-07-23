3dc3fd5c2d7064fe1d5efc828cf7d38d827c7ca8
LiutaoXiang <xiangliutao@foxmail.com>
2025-07-23 17:38:14 +0800

    修改OpenFeign日志级别

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,13 +1,14 @@
 package com.hmall.cart;
 
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-@EnableFeignClients(basePackages = "com.hmall.api.client") //指定扫描包，要不然无法自动装配
+                                                            /// 修改feign的日志级别
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class) //指定扫描包，要不然无法自动装配
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
