39b1fac6ca8c9c4be38b4c819f617669fffef58b
LiutaoXiang <xiangliutao@foxmail.com>
2025-07-25 17:06:22 +0800

    添加微服务保护机制，访问购物车时，如果访问item失败，自动调用fallback

--- item-service/src/main/java/com/hmall/item/controller/ItemController.java
+++ item-service/src/main/java/com/hmall/item/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.hmall.item.controller;
 
 
+import cn.hutool.core.thread.ThreadUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
@@ -44,6 +45,7 @@ public class ItemController {
     @ApiOperation("根据id查询商品")
     @GetMapping("{id}")
     public ItemDTO queryItemById(@PathVariable("id") Long id) {
+
         return BeanUtils.copyBean(itemService.getById(id), ItemDTO.class);
     }
 
